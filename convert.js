01cf11d84f9106d2967b4bedeb683ccf9104b929
Gedas the Evil <gediminas.skucas@gmail.com>
2017-08-19 10:17:59 +0300

    Writing the coefifients for data re-conversion

--- convert.js
+++ convert.js
@@ -1,9 +1,29 @@
-/*jslint browser: false */
-/*global require */
+/*jslint browser: false, nodejs: true */
+/*global require, process */
 
-var modelName = 'tchest.mdl',
-    readerBase = require('./lib/MdlDataNormalization.js'),
-    writerBase = require('./lib/MdlBinaryWriter.js'),
+var argsGet = (function () {
+    var index = 1;
+
+    return function (fallback) {
+        ++index;
+        return process.argv[index] !== undefined ? process.argv[index] : fallback;
+    };
+} ());
+
+var modelName = argsGet('tchest.mdl'),
+    mode = argsGet('toBin'),
+    readerBase,
+    writerBase,
+    data;
+
+if (mode === 'toBin') {
+    readerBase = require('./lib/MdlDataNormalization.js');
+    writerBase = require('./lib/MdlBinaryWriter.js');
     data = new readerBase(['in', modelName].join('/'));
 
-new writerBase(data.getNormalized(), ['out/', modelName, '.bin'].join(''));
+    new writerBase(data.getNormalized(), ['out/', modelName, '.bin'].join(''));
+}
+
+if (mode === 'toJson') {
+
+}
